7d27c531102d8412115a4a3c728dd594d636fd95
Keith <Keith.macario@gmail.com>
2021-12-12 02:44:30 -0800

    First test for destroyerHealth passed.

--- src/ships.js
+++ src/ships.js
@@ -1,8 +1,28 @@
 
-const destroyerShip = () => {
-    destroyerSize = [1,2]
-    destroyerHealth = 2;
-    destroyerSunk = false;
+const DestroyerShip = () => {
+   const destroyerLength = [0,0]
+   let destroyerHealth = 2;
+   let destroyerSunk = false;
+
+   function damageTaken (x){
+    destroyerHealth -= x;
+    if(destroyerHealth <= 0){
+        youSunkMyShip();
+    }
+    return destroyerHealth;
+   }
+
+//    function add (x,y) {
+//        return (x + y);
+//    }
+
+    return {destroyerLength, destroyerHealth, destroyerSunk, damageTaken}
 }
 
-let playerTwoDestroyer = destroyerShip();
+function youSunkMyShip(ship){
+    console.log("You sunk my ship!");
+}
+
+let PlayerTwoDestroyer = DestroyerShip();
+
+module.exports = PlayerTwoDestroyer;
--- src/ships.test.js
+++ src/ships.test.js
@@ -1,1 +1,10 @@
-const shipTest = require('./ships.js')
+const PlayerTwoDestroyer = require('./ships.js')
+
+test(' destroyer ', () => {
+    expect(PlayerTwoDestroyer.damageTaken(2)).toBe(0);
+});
+
+
+// test(' destroyer ', () => {
+//     expect(PlayerTwoDestroyer.add(2,2)).toBe(4);
+// });
