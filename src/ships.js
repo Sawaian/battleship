3501161925766193ed66de8e4a9957e91bbdc4bf
Keith <Keith.macario@gmail.com>
2021-12-26 19:38:39 -0800

    Redoing Todo and writing test before function. Tested ship takes one point of damage.

--- src/ships.js
+++ src/ships.js
@@ -1,47 +1,65 @@
 
-const DestroyerShip = () => {
-   let destroyerLength = [0,0]
-   let destroyerHealth = 2;
-   let destroyerSunk = false;
-
-   let cruiserLength = ['hit', 'hit', 'hit']
-
-   function hit(x){
-    //mark a position on a destroyerLength.
-
-    if(x === 1){
-        destroyerLength[0] = 'hit';
-        console.log(destroyerLength[0]);
-        return destroyerLength[0];
+class Ship {
+    constructor(health){
+    this.health = health;
+    this.length = [0,0]
+    this.sunk = false;
     }
-    if(x === 2){
-        destroyerLength[1] =  'hit';
-        console.log("second");
-        return destroyerLength[1];
-    }
-   }
 
-    function checkHealth(ship){
-        let hitScored = 0;
-        let sunk = false;
+
+
+    takenDamage(ship, damage){
+      return ship - damage;
+    }
+}
+
+let PlayerOneDestroyer = new Ship(2);
+
+
+
+// const DestroyerShip = () => {
+//    let destroyerLength = [0,0]
+//    let destroyerHealth = 2;
+//    let destroyerSunk = false;
+
+//    let cruiserLength = ['hit', 'hit', 'hit']
+
+//    function hit(x){
+//     //mark a position on a destroyerLength.
+
+//     if(x === 1){
+//         destroyerLength[0] = 'hit';
+//         console.log(destroyerLength[0]);
+//         return destroyerLength[0];
+//     }
+//     if(x === 2){
+//         destroyerLength[1] =  'hit';
+//         console.log("second");
+//         return destroyerLength[1];
+//     }
+//    }
+
+//     function checkHealth(ship){
+//         let hitScored = 0;
+//         let sunk = false;
       
-        for(let i = 0; i < ship.length; i++){
-            if(ship[i] === 'hit'){
-                hitScored++
-                if(hitScored === ship.length){
-                    return sunk = true;
-                }
-            }
-            else{ return sunk = false}
-        }
-    }
-    return {destroyerLength, destroyerHealth, destroyerSunk, hit, checkHealth,cruiserLength}
-}
+//         for(let i = 0; i < ship.length; i++){
+//             if(ship[i] === 'hit'){
+//                 hitScored++
+//                 if(hitScored === ship.length){
+//                     return sunk = true;
+//                 }
+//             }
+//             else{ return sunk = false}
+//         }
+//     }
+//     return {destroyerLength, destroyerHealth, destroyerSunk, hit, checkHealth,cruiserLength}
+// }
 
-function youSunkMyShip(ship){
-    console.log("You sunk my ship!");
-}
+// function youSunkMyShip(ship){
+//     console.log("You sunk my ship!");
+// }
 
-let PlayerTwoDestroyer = DestroyerShip();
+// let PlayerTwoDestroyer = DestroyerShip();
 
-module.exports = PlayerTwoDestroyer;
+module.exports = PlayerOneDestroyer
--- src/ships.test.js
+++ src/ships.test.js
@@ -1,15 +1,20 @@
-const PlayerTwoDestroyer = require('./ships.js')
+// const PlayerTwoDestroyer = require('./ships.js')
+const PlayerOneDestroyer = require('./ships.js')
 
-test(' destroyer ', () => {
-    expect(PlayerTwoDestroyer.hit(1)).toBe('hit');
-});
+// test(' destroyer ', () => {
+//     expect(PlayerTwoDestroyer.hit(1)).toBe('hit');
+// });
 
-test(' destroyer ', () => {
-    expect(PlayerTwoDestroyer.hit(2)).toBe('hit');
-});
+// test(' destroyer ', () => {
+//     expect(PlayerTwoDestroyer.hit(2)).toBe('hit');
+// });
 
-test(' cruiser ', ()=> {
-    expect(PlayerTwoDestroyer.checkHealth(PlayerTwoDestroyer.destroyerHealth)).toBe(false);
+// test(' cruiser ', ()=> {
+//     expect(PlayerTwoDestroyer.checkHealth(PlayerTwoDestroyer.destroyerLength)).toBe(true);
+// })
+
+test(' ship hit! ', () =>{
+    expect(PlayerOneDestroyer.takenDamage(PlayerOneDestroyer.health,1)).toBe(1);
 })
 
 // test(' destroyer ', () => {n
